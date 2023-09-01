229b5ba09bd04a885273cea4398cbde58c22c771
2044smile <2044smile@naver.com>
2023-09-01 21:24:57 +0900

    :recycle: Refactor: minor

--- get_test_data.py
+++ get_test_data.py
@@ -5,6 +5,23 @@ import pandas as pd
 from googleapiclient.discovery import build
 
 
+def make_csv(playlist: list, channel_name: str):
+    lst_title = []
+    lst_thumbnails = []
+
+    for pl in playlist:
+        lst_title.append(pl['snippet']['title'])
+        lst_thumbnails.append(pl['snippet']['thumbnails']['default']['url'])
+
+    df = pd.DataFrame(
+        {
+            "title": lst_title,
+            "thumbnails": lst_thumbnails
+        }
+    )
+    df.to_csv(f"/Users/cslee/vscode/get-youtube-data/csv/{channel_name}.csv")
+
+
 class YouTube:
     def __init__(self):
         developer_key = os.getenv('YOUTUBE_DEVELOPER_KEY')
@@ -21,22 +38,6 @@ class YouTube:
 
         return playlist['items']
 
-    def make_csv(self, playlist: list, channel_name: str):
-        lst_title = []
-        lst_thumbnails = []
-
-        for pl in playlist:
-            lst_title.append(pl['snippet']['title'])
-            lst_thumbnails.append(pl['snippet']['thumbnails']['default']['url'])
-
-        df = pd.DataFrame(
-            {
-                "title": lst_title,
-                "thumbnails": lst_thumbnails
-            }
-        )
-        df.to_csv(f"/Users/cslee/vscode/get-youtube-data/csv/{channel_name}.csv")
-
 
 parser = argparse.ArgumentParser(description="유튜브 채널 이름을 입력해 주세요. EX) 곽튜브, 빠니보틀")
 parser.add_argument('-c', '--channel')
@@ -45,10 +46,10 @@ args = parser.parse_args()
 youtube = YouTube()
 if args.channel == '빠니보틀':
     target = youtube.youtuber(channel_id='UCNhofiqfw5nl-NeDJkXtPvw')
-    youtube.make_csv(target, channel_name='빠니보틀')
+    make_csv(target, channel_name='빠니보틀')
 elif args.channel == '곽튜브':
     target = youtube.youtuber(channel_id='UClRNDVO8093rmRTtLe4GEPw')
-    youtube.make_csv(target, channel_name='곽튜브')
+    make_csv(target, channel_name='곽튜브')
 else:
     print("유튜브 채널 이름을 입력해 주세요.")
 

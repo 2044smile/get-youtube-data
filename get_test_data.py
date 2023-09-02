87867b0df1064a0a15635489f946ba1192afd84d
2044smile <2044smile@naver.com>
2023-09-02 15:12:29 +0900

    :sparkles: Feat: get_my_selected_youtube_playlists
    parser add number and 유튜브 재생목록에서 본인이 가져오고 싶은 번호 EX) 첫 번째 영상 -n 0

--- get_test_data.py
+++ get_test_data.py
@@ -29,7 +29,13 @@ class YouTube:
         youtube_api_version = 'v3'
         self.youtube = build(youtube_api_service_name, youtube_api_version, developerKey=developer_key)
 
-    def youtuber(self, channel_id: str) -> list:
+    def get_all_youtube_playlists(self, channel_id: str) -> list:
+        """
+        :param channel_id:
+        :return: googleapiclient playlist['items']
+
+        유튜버가 가지고 있는 재생목록의 플레이 리스트를 제공합니다.
+        """
         playlist = self.youtube.playlists().list(
             channelId=channel_id,
             part='snippet',
@@ -38,18 +44,36 @@ class YouTube:
 
         return playlist['items']
 
+    def get_my_selected_youtube_playlists(self, playlist_id: str):
+        playlist_items = self.youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            playlistId=playlist_id,
+            maxResults=50
+        ).execute()
+
+        return playlist_items
+
 
 parser = argparse.ArgumentParser(description="유튜브 채널 이름을 입력해 주세요. EX) 곽튜브, 빠니보틀")
 parser.add_argument('-c', '--channel')
+parser.add_argument('-n', '--number')
 args = parser.parse_args()
 
 youtube = YouTube()
 if args.channel == '빠니보틀':
-    target = youtube.youtuber(channel_id='UCNhofiqfw5nl-NeDJkXtPvw')
+    target = youtube.get_all_youtube_playlists(channel_id='UCNhofiqfw5nl-NeDJkXtPvw')
     make_csv(target, channel_name='빠니보틀')
+    if args.number:
+        youtube.get_my_selected_youtube_playlists(
+            playlist_id=target[int(args.number)]['id']
+        )
 elif args.channel == '곽튜브':
-    target = youtube.youtuber(channel_id='UClRNDVO8093rmRTtLe4GEPw')
+    target = youtube.get_all_youtube_playlists(channel_id='UClRNDVO8093rmRTtLe4GEPw')
     make_csv(target, channel_name='곽튜브')
+    if args.number:
+        youtube.get_my_selected_youtube_playlists(
+            playlist_id=target[int(args.number)]['id']
+        )
 else:
     print("유튜브 채널 이름을 입력해 주세요.")
 
